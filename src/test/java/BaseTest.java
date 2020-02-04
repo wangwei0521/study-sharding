0f9348018c503683f32a3b70807f10ed89594882
wangwei0521 <18203669834@163.com>
2020-02-04 10:49:44 +0800

    测试idea上传到github

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,7 +7,8 @@ public class BaseTest {
     @Test
     public void test() {
         Assert.assertTrue("这只是一个测试！", LocalDate.now().getYear() < Integer.MAX_VALUE);
-	System.out.println("测试上传到GitHub");
-	System.out.println("测试git pull");
+        System.out.println("测试上传到GitHub");
+        System.out.println("测试git pull");
+        System.out.println("测试用idea上传至github");
     }
 }
